4061c88d7662e847289e15df6e9c17e7ecaa3e62
ChristopherGammaWau <christopher.clement.true@outlook.com>
2025-12-08 10:38:08 +0700

    Update video 3.3

--- WordGuessing/src/shaunwassel/learning/WordGuessingMain.java
+++ WordGuessing/src/shaunwassel/learning/WordGuessingMain.java
@@ -1,5 +1,6 @@
 package shaunwassel.learning;
 
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -26,16 +27,46 @@ public class WordGuessingMain {
 	 * Starts the console game.
 	 * */
 	public void Play() {
+		// TEST See what word is being guessed
+		System.out.println("Word being guessed: " + this.wordToGuess);
 		Scanner userInputScanner = new Scanner(System.in);
 		
 		// Get user guesses
 		while(this.attemptsRemaining > 0) {
 			System.out.println(String.format("You have %d attempts left..", this.attemptsRemaining));
 			System.out.print("Guess a letter: ");
+			try {
+				char charGuess = userInputScanner.nextLine().charAt(0);
+				if (tryGuessLetter(charGuess)) {
+					System.out.println("Right guess.");
+				}
+				else {
+					this.attemptsRemaining--;
+					System.out.println("Wrong guess!!");
+					
+				}
+			}
+			catch (NoSuchElementException e) {
+				System.out.println("Please enter 1 valid character!!");
+			}
 			
 			
-			this.attemptsRemaining--;
 		}
+	}
+	
+	/**
+	 * Checks if letter from user input is found in the word (case-insensitive).
+	 * Returns true is a letter is found in the word.
+	 */
+	private boolean tryGuessLetter(char letter) {
+		boolean letterFound = false;
+		for (int i = 0; i < this.wordToGuess.length(); i++) {
+			if (Character.toLowerCase(this.wordToGuess.charAt(i)) == Character.toLowerCase(letter)) {
+				this.guessedLetters[i] = letter;
+				letterFound = true;
+			}
+		}
+		return letterFound;
 	}
 
 }
