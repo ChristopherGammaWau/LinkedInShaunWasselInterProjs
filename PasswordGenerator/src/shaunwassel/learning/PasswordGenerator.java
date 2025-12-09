a831b02a3fe82696890a5bd6c8b5d3808458de41
ChristopherGammaWau <christopher.clement.true@outlook.com>
2025-12-09 08:48:02 +0700

    Update: Proj 3 bugfix TextBasedInterface and solution to challenge

--- PasswordGenerator/src/shaunwassel/learning/PasswordGenerator.java
+++ PasswordGenerator/src/shaunwassel/learning/PasswordGenerator.java
@@ -9,21 +9,54 @@ public class PasswordGenerator {
 	String specialChars = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
 	String numberChars = "0123456789";
 	
+	/**
+	 * Checks if s2 contains a character from s1
+	 * @param s1: String 
+	 * @param s2: String
+	 * @return: Boolean, true if overlap
+	 */
+	private boolean hasOverlap(String s1, String s2) {
+		for (char c : s1.toCharArray()) {
+			if (s2.contains(String.valueOf(c))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	String GeneratePassword(int length, boolean useUpperCase, boolean useLowerCase, boolean useNumbers, boolean useSpecialChars) {
 		
+		boolean doPasswordFitCriteria = false;
 		Random rand = new Random();
-		StringBuilder password = new StringBuilder();
+		
 		StringBuilder charPoolBuilder = new StringBuilder();
 		charPoolBuilder.append(useUpperCase ? this.uppercaseChars : ""); 
 		charPoolBuilder.append(useLowerCase ? this.lowercaseChars : ""); 
 		charPoolBuilder.append(useNumbers ? this.numberChars : ""); 
 		charPoolBuilder.append(useSpecialChars ? this.specialChars : ""); 
-
 		String charPool = charPoolBuilder.toString();
-		// For loop to select characters
-		for(int i = 0; i < length; i++) {
-			password.append(charPool.charAt(rand.nextInt(charPool.length())));
-		}
+		StringBuilder password;
+		// Solution: Check if password fits criteria, regenerate if not
+		do {
+			password = new StringBuilder();
+			
+			// For loop to select characters
+			for(int i = 0; i < length; i++) {
+				password.append(charPool.charAt(rand.nextInt(charPool.length())));
+			}
+			
+			// TODO: Improve nested if statements
+			if(this.hasOverlap(this.lowercaseChars, password.toString())) {
+				if(this.hasOverlap(this.uppercaseChars, password.toString())) {
+					if(this.hasOverlap(this.numberChars, password.toString())) {
+						if(this.hasOverlap(this.specialChars, password.toString())) {
+							doPasswordFitCriteria = true;
+						}
+					}
+				}
+			}
+			
+		} while (!doPasswordFitCriteria);
 		
 		return password.toString();
 	}
--- PasswordGenerator/src/shaunwassel/learning/TextBasedInterface.java
+++ PasswordGenerator/src/shaunwassel/learning/TextBasedInterface.java
@@ -16,11 +16,12 @@ public class TextBasedInterface implements UserInterface{
 
 	@Override
 	public int getPasswordLength() {
-		System.out.println("Please enter password length: ");
+		System.out.print("Please enter password length: ");
 		int length = 0;
 		while(!(length > 0)) {
 			try {
 				length = this.scanner.nextInt();
+				this.scanner.nextLine(); // Consumes leftover '\n' from nextInt
 			}
 			catch(InputMismatchException e){
 				System.out.println("Please enter a valid number!");
@@ -31,7 +32,7 @@ public class TextBasedInterface implements UserInterface{
 
 	@Override
 	public boolean useLowerCase() {
-		System.out.println("Use lowercase characters? (y/n): ");
+		System.out.print("Use lowercase characters? (y/n): ");
 		boolean isInvalidInput = true;
 		boolean charFlag = false;
 		try {
@@ -40,17 +41,17 @@ public class TextBasedInterface implements UserInterface{
 				char userInput = scanner.nextLine().toLowerCase().charAt(0);
 				switch (userInput) {
 				case 'y':
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = true;
 					break;
 				case 'n':
 					System.out.println("Goodbye!");
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = false;
 					break;
 				default:
 					System.out.println("Please enter \'Y\' or \'N\'");
-					isInvalidInput = false;
+					isInvalidInput = true;
 				}
 
 			}while (isInvalidInput);
@@ -62,7 +63,7 @@ public class TextBasedInterface implements UserInterface{
 
 	@Override
 	public boolean useUpperCase() {
-		System.out.println("Use uppercase characters? (y/n): ");
+		System.out.print("Use uppercase characters? (y/n): ");
 		boolean isInvalidInput = true;
 		boolean charFlag = false;
 		try {
@@ -71,17 +72,17 @@ public class TextBasedInterface implements UserInterface{
 				char userInput = scanner.nextLine().toLowerCase().charAt(0);
 				switch (userInput) {
 				case 'y':
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = true;
 					break;
 				case 'n':
 					System.out.println("Goodbye!");
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = false;
 					break;
 				default:
 					System.out.println("Please enter \'Y\' or \'N\'");
-					isInvalidInput = false;
+					isInvalidInput = true;
 				}
 
 			}while (isInvalidInput);
@@ -93,7 +94,7 @@ public class TextBasedInterface implements UserInterface{
 
 	@Override
 	public boolean useNumbers() {
-		System.out.println("Use number characters? (y/n): ");
+		System.out.print("Use number characters? (y/n): ");
 		boolean isInvalidInput = true;
 		boolean charFlag = false;
 		try {
@@ -102,17 +103,17 @@ public class TextBasedInterface implements UserInterface{
 				char userInput = scanner.nextLine().toLowerCase().charAt(0);
 				switch (userInput) {
 				case 'y':
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = true;
 					break;
 				case 'n':
 					System.out.println("Goodbye!");
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = false;
 					break;
 				default:
 					System.out.println("Please enter \'Y\' or \'N\'");
-					isInvalidInput = false;
+					isInvalidInput = true;
 				}
 
 			}while (isInvalidInput);
@@ -124,7 +125,7 @@ public class TextBasedInterface implements UserInterface{
 
 	@Override
 	public boolean useSpecialChars() {
-		System.out.println("Use special characters? (y/n): ");
+		System.out.print("Use special characters? (y/n): ");
 		boolean isInvalidInput = true;
 		boolean charFlag = false;
 		try {
@@ -133,17 +134,17 @@ public class TextBasedInterface implements UserInterface{
 				char userInput = scanner.nextLine().toLowerCase().charAt(0);
 				switch (userInput) {
 				case 'y':
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = true;
 					break;
 				case 'n':
 					System.out.println("Goodbye!");
-					isInvalidInput = true;
+					isInvalidInput = false;
 					charFlag = false;
 					break;
 				default:
 					System.out.println("Please enter \'Y\' or \'N\'");
-					isInvalidInput = false;
+					isInvalidInput = true;
 				}
 
 			}while (isInvalidInput);
