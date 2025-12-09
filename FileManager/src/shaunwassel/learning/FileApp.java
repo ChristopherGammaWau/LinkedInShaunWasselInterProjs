4cdc222a1285fd68da5148e33582faf6e1d50ec8
ChristopherGammaWau <christopher.clement.true@outlook.com>
2025-12-09 12:20:32 +0700

    Update: Implements challenge solution proj 4

--- FileManager/src/shaunwassel/learning/FileApp.java
+++ FileManager/src/shaunwassel/learning/FileApp.java
@@ -1,10 +1,40 @@
 package shaunwassel.learning;
 
-public class FileApp {
+public class FileApp implements UIEventHandler {
+	
+	FileManager app = null;
+	
+	public FileApp() {
+		app = new FileManager();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		TextBasedInterface appInterface = new TextBasedInterface();
+		appInterface.subscribe(new FileApp());
+		appInterface.start();
 
+	}
+
+	@Override
+	public void onDirectoryList(String directoryPath) {
+		
+		this.app.listFiles(directoryPath);
+		
+	}
+
+	@Override
+	public void onCreateDirectory(String directoryPath) {
+		
+		this.app.createDirectory(directoryPath);
+		
+	}
+
+	@Override
+	public void onDeleteDirectoryOrFile(String directoryOrFilePath) {
+		
+		this.app.deleteDirectoryOrFile(directoryOrFilePath);
+		
 	}
 
 }
