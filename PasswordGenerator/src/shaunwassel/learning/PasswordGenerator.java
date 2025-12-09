cb2c09469669bba49c9631089ffe5271a38adad6
ChristopherGammaWau <christopher.clement.true@outlook.com>
2025-12-09 08:29:28 +0700

    Update: Video 4.3-4.6

--- PasswordGenerator/src/shaunwassel/learning/PasswordGenerator.java
+++ PasswordGenerator/src/shaunwassel/learning/PasswordGenerator.java
@@ -1,5 +1,7 @@
 package shaunwassel.learning;
 
+import java.util.Random;
+
 public class PasswordGenerator {
 	
 	String lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
@@ -9,14 +11,21 @@ public class PasswordGenerator {
 	
 	String GeneratePassword(int length, boolean useUpperCase, boolean useLowerCase, boolean useNumbers, boolean useSpecialChars) {
 		
-
+		Random rand = new Random();
 		StringBuilder password = new StringBuilder();
-		
+		StringBuilder charPoolBuilder = new StringBuilder();
+		charPoolBuilder.append(useUpperCase ? this.uppercaseChars : ""); 
+		charPoolBuilder.append(useLowerCase ? this.lowercaseChars : ""); 
+		charPoolBuilder.append(useNumbers ? this.numberChars : ""); 
+		charPoolBuilder.append(useSpecialChars ? this.specialChars : ""); 
+
+		String charPool = charPoolBuilder.toString();
 		// For loop to select characters
 		for(int i = 0; i < length; i++) {
-			
+			password.append(charPool.charAt(rand.nextInt(charPool.length())));
 		}
 		
+		return password.toString();
 	}
 
 }
